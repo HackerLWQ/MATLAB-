86659c729e7c486fa13e60cfc3a89f8d8a830ce5
HIT-VincentLiu <112791550+HackerLWQ@users.noreply.github.com>
2023-07-23 20:04:53 +0800

    Delete MATLAB程序设计/P18 单一输出变量的函数+变量使用范围+子函数 directory

--- MATLAB程序设计/P18 单一输出变量的函数+变量使用范围+子函数/course.m
+++
@@ -1,8 +0,0 @@
-1.函数定义
-function outputArgument = functionName(inputArgument)
-
-2.函数向量化运算：sin([1,2,3,4,5])
-
-3.函数传参和调用
-
-4.子函数:局部函数
--- MATLAB程序设计/P18 单一输出变量的函数+变量使用范围+子函数/test.m
+++
@@ -1,2 +0,0 @@
-radius = 5;
-fprintf('半径为%f的圆的面积为%f. \n',radius,calcarea(radius));
